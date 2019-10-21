6bbb712cc4857fb2125986655668d5c1512421f1
Oliver Faust <oli_fau@hotmail.com>
2019-10-21 17:53:09 +0200

    added peer2peer server websocket

---
+++ app/p2p-server.js
@@ -0,0 +1,50 @@
+const Websocket = require('ws');
+
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+//ws://localhost:5001,ws://localhost:5002 npm run dev
+
+class P2pServer{
+    constructor(blockchain){
+        this.blockchain = blockchain;
+        this.sockets = [];
+
+    }
+
+    listen(){
+        const server = new Websocket.Server({port: P2P_PORT});
+        server.on('connection',socket => this.connectSocket(socket));
+
+        this.connectToPeers();
+
+        console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+    }
+
+    connectToPeers(){
+        peers.forEach(peer => {
+            //ws://localhost:5001,w
+            const socket = new Websocket(peer);
+
+            socket.on('open', () => this.connectSocket(socket));
+        });
+    }
+
+    connectSocket(socket){
+        this.sockets.push(socket);
+        console.log('Socket connected');
+
+        this.messageHandler(socket);
+
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    messageHandler(socket){
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+            console.log('data',data);
+        });
+    }
+}
+
+module.exports = P2pServer;
