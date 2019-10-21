b47703cdbe735c5e7c5dcef40723d720883f7f3f
Oliver Faust <oli_fau@hotmail.com>
2019-10-21 20:09:42 +0200

    implemented proof of work

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -8,6 +8,7 @@ describe('Block', () => {
         lastBlock = Block.genesis();
         block = Block.mineBlock(lastBlock,data);
     });
+
     it('sets the `data` to match the input', () => {
         expect(block.data).toEqual(data);
 
@@ -15,5 +16,18 @@ describe('Block', () => {
     
     it('sets the `lastHash` to match the hash of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('generates a hash that matches the difficulty', () => {
+        expect(block.hash.substring(0,block.difficulty)).toEqual('0'.repeat(block.difficulty));
+        console.log(block.toString());
+    });
+
+    it('lower the difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp+360000)).toEqual(block.difficulty-1);
+    });
+
+    it('raises the difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp+1)).toEqual(block.difficulty+1);
     });
 });
